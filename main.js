b2f0d2cb78c8455c479600ab30c9129c9829f094
Rodrigo Figueira <rodrigoservice@gmail.com>
2020-12-01 10:21:51 -0300

    added function to compare date

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-class App{
-
-    test(){
-        console.log('ok');
-    }
-
-}
+var strData = "02/12/2020";
+var partesData = strData.split("/");
+var data = new Date(partesData[2], partesData[1] - 1, partesData[0]);
+if(data > new Date())
+   document.body.innerHTML = '';
