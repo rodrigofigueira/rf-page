a4777eac91fe06fc92ec4f9d6f2636b4b7f3e776
Rodrigo Figueira <rodrigoservice@gmail.com>
2020-12-01 10:24:42 -0300

    update function to compare date

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var strData = "02/12/2020";
 var partesData = strData.split("/");
 var data = new Date(partesData[2], partesData[1] - 1, partesData[0]);
-if(data > new Date())
+if(data < new Date())
    document.body.innerHTML = '';
